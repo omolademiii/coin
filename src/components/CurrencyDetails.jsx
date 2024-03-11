db4765ef09d10244d9758ed0129c5add07695c3d
omolade <tosin.ogunmola44@gmail.com>
2024-03-11 11:07:04 +0100

    search

--- src/components/CurrencyDetails.jsx
+++ src/components/CurrencyDetails.jsx
@@ -32,9 +32,52 @@ const CurrencyDetails = () => {
   return (
     <div className='px-[150px] py-[40px] flex justify-between'>
       <div className='w-[500px]'>
+      <div className='flex justify-between'>
       <div className='flex gap-3'>
-      <img src={currencyDetails?.image.thumb} alt="" />
-      <h2 className='text-2xl font-semibold'>{currencyDetails?.name}</h2>
+      <img src={currencyDetails?.image.small} alt="" />
+      <h2 className='text-2xl font-semibold mt-2'>{currencyDetails?.name}</h2>
+      </div>
+      <form className="max-w-md mx-auto">
+  <label
+    htmlFor="default-search"
+    className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+  >
+    Search
+  </label>
+  <div className="relative">
+    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <svg
+        className="w-4 h-4 text-gray-500 dark:text-gray-400"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 20 20"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+        />
+      </svg>
+    </div>
+    <input
+      type="search"
+      id="default-search"
+      className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="currency"
+      required=""
+    />
+    <button
+      type="submit"
+      className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Search
+    </button>
+  </div>
+</form>
+
       </div>
       <p className='text-4xl font-bold mt-4'>${currencyDetails?.market_data.current_price.usd.toLocaleString()}</p>
       <h2 className='mt-4 text-lg font-medium'> twitter followers : { currencyDetails?.community_data.twitter_followers.toLocaleString()}</h2>
