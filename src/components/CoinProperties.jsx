7e86773f04df882cd766360288c4c3c7583356ee
omolade <tosin.ogunmola44@gmail.com>
2024-03-28 22:11:59 +0100

    this

--- src/components/CoinProperties.jsx
+++ src/components/CoinProperties.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SingleCoinList from "./SingleCoinList";
-import { useNavigate } from "react-router-dom";
 
 const CoinProperties = () => {
   const [currencies, setCurrencies] = useState(null);
